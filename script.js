0e1b911368064644568b57db39efbbde38f690ab
Maciej <38870449+maciej-wieczorek@users.noreply.github.com>
2019-05-28 10:52:21 +0200

    script

---
+++ script.js
@@ -0,0 +1,8 @@
+function padding () {
+    var slider = document.querySelector('.container');
+    var navbar = document.querySelector('.navbar');
+    var padd = parseFloat(getComputedStyle(slider).paddingLeft) + parseFloat(getComputedStyle(slider).marginLeft);
+
+    navbar.style.paddingLeft = padd + "px";
+    navbar.style.paddingRight = padd + "px";
+}
